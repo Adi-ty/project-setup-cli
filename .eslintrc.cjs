997656b166b387e9ae9c54ccfd436bfadc196556
Aditya <adityasinghboss1234@gmail.com>
2023-08-18 20:39:57 +0530

    Initial Commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,25 @@
+module.exports = {
+  env: {
+    es2021: true,
+    node: true,
+  },
+  extends: ['eslint:recommmended', 'plugin:prettier/recommended'],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: ['.eslintrc.{js,cjs}'],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+    'prettier/prettier': 'error',
+  },
+};
---
+++ index.js
@@ -0,0 +1,33 @@
+#!/usr/bin/env node
+
+import inquirer from 'inquirer';
+import * as fs from 'fs';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const CHOICES = fs.readdirSync(`${__dirname}/templates`);
+
+const QUESTIONS = [
+  {
+    name: 'project-choice',
+    type: 'list',
+    message: 'Which project template would you like to generate?',
+    choices: CHOICES,
+  },
+  {
+    name: 'project-name',
+    type: 'input',
+    message: 'Project Name:',
+    validate: function (input) {
+      if (/^([A-Za-z\-\\_\d])+$/.test(input)) return true;
+      else
+        return 'Project name may only include letters, numbers, underscores and hashes.';
+    },
+  },
+];
+
+inquirer.prompt(QUESTIONS).then(answers => {
+  console.log(answers);
+});
